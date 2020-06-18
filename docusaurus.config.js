af0fe80969afd8a2f98dcf415c39408d5a40f084
iurcovici <65172880+iurcovici@users.noreply.github.com>
2020-06-18 07:47:42 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,7 +75,7 @@ module.exports = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Arduino Projects. Built with Docusaurus.`,
     },
   },
   presets: [
